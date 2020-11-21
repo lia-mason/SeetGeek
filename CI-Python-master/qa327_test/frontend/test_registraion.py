1013986698eba7f8a5393345c8a8061d45bf0da3
aartisinghh <44043481+aartisinghh@users.noreply.github.com>
2020-11-20 23:38:41 -0500

    Update test_registraion.py

--- CI-Python-master/qa327_test/frontend/test_registraion.py
+++ CI-Python-master/qa327_test/frontend/test_registraion.py
@@ -56,6 +56,93 @@ class FrontEndHomePageTest(BaseCase):
         self.assert_element("#welcome-header")
         # test for welcome header message ("Hi {username}")
         self.assert_text("Hi test_frontend", "#welcome-header")
+        
+     @patch('qa327.backend.get_user', return_value=test_user)
+     @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
+     def test_login(self, *_):
+        """
+        R3.1 This will test the login page and test if the user gets redirected to login page if not logged in
+        """
+        self.open(base_url + '/logout')
+        self.open(base_url + '/login')
+        self.assert_element("#title")
+        self.assert_text("Log in", "#title")
+        
+        #test that logout redirects to '/'
+        URL = self.get_current_url();
+        self.assert_equal(URL, base_url + '/'); 
+        
+    @patch('qa327.backend.get_user', return_value=test_user)
+    @patch('qa327.backend.get_all_tickets', return_value=test_tickets)    
+    def test_show_user_balance(self, *_):
+        """
+        R3.3 This will test whether the user balance displays
+        """
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "test_frontend")
+        self.click('input[type="submit"]')
+        self.open(base_url)
+        # test if the page loads correctly
+        self.assert_element("#welcome-header")
+        self.assert_element("#ubalance")
+        
+    @patch('qa327.backend.get_user', return_value=test_user)
+    @patch('qa327.backend.get_all_tickets', return_value=test_tickets)    
+    def test_all_tickets(self, *_):  
+         # open login page
+         self.open(base_url + '/login')
+         # fill email and password
+         self.type("#email", "test_frontend@test.com")
+         self.type("#password", "test_frontend")
+         # click enter button
+         self.click('input[type="submit"]')
+    
+         # after clicking on the browser (the line above)
+         # the front-end code is activated
+         # and tries to call get_user function.
+         # The get_user function is supposed to read data from database
+         # and return the value. However, here we only want to test the
+         # front-end, without running the backend logics.
+         # so we patch the backend to return a specific user instance,
+         # rather than running that program. (see @ annotations above)
+    
+    
+         # open home page
+         self.open(base_url)
+         # test if the page loads correctly
+         self.assert_element("#welcome-header")
+         self.assert_text("Welcome test_frontend", "#welcome-header")
+         self.assert_element("#tickets")
+         self.assert_text("Here are all available tickets")
+        
+        
+    @patch('qa327.backend.get_user', return_value=test_user)
+    @patch('qa327.backend.get_all_tickets', return_value=test_tickets)    
+    def test_buy_form(self, *_):  
+         """
+         This will test if buy form is displaying
+         """
+         # open login page
+         self.open(base_url + '/login')
+         # fill email and password
+         self.type("#email", "test_frontend@test.com")
+         self.type("#password", "test_frontend")
+         # click enter button
+         self.click('input[type="submit"]')
+
+         # open home page
+         self.open(base_url)
+         # test if the page loads correctly
+         self.assert_element("#welcome-header")
+
+         self.assert_element("#name")
+         self.type("#name")
+         self.assert_element("#quantity")
+         self.type("#quantity", "1")
+         self.click("#t-submit")
+         self.assert_element("#welcome-header")
+       
 
 #     @patch('qa327.backend.get_user', return_value=test_user)
 #     @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
