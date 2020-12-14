cc15c136226cd81e8a6396871f229f0a542c0527
AmaarJ <amaar.jivanji001@gmail.com>
2020-12-15 01:36:32 +0300

    integration test cases

--- CI-Python-master/qa327_test/integration/test_registration.py
+++ CI-Python-master/qa327_test/integration/test_registration.py
@@ -13,23 +13,65 @@ class Registered(BaseCase):
     def register(self):
         """register new user"""
         self.open(base_url + '/register')
-        self.type("#email", "test0")
-        self.type("#name", "test0")
-        self.type("#password", "test0")
-        self.type("#password2", "test0")
+        self.type("#email", "amaarjivanji@gmail.com")
+        self.type("#name", "Amaar Jivanji")
+        self.type("#password", "KingstonOntario2$")
+        self.type("#password2", "KingstonOntario2$")
         self.click('input[type="submit"]')
 
     def login(self):
         """ Login to Swag Labs and verify that login was successful. """
         self.open(base_url + '/login')
-        self.type("#email", "test0")
-        self.type("#password", "test0")
+        self.type("#email", "amaarjivanji@gmail.com")
+        self.type("#password", "KingstonOntario2$")
         self.click('input[type="submit"]')
 
-    # def test_register_login(self):
-    #     """ This test checks the implemented login/logout feature """
-    #     self.register()
-    #     self.login()
-    #     self.open(base_url)
-    #     self.assert_element("#welcome-header")
-    #     self.assert_text("Welcome test0", "#welcome-header")
+    def create_tickets(self):
+        """Adds a new ticket through sell form. """
+        self.open(base_url)
+        self.type("#tname", "Avengers Infinity War")
+        self.type("#tquantity", "20")
+        self.type("#tprice", "15")
+        self.type("#texpiration", "20210301")
+        self.click("#sell-btn-submit")
+    
+    def create_tickets_2(self):
+        """Adds a new ticket through sell form. """
+        self.open(base_url)
+        self.type("#tname", "Superman")
+        self.type("#tquantity", "20")
+        self.type("#tprice", "15")
+        self.type("#texpiration", "20210711")
+        self.click("#sell-btn-submit")
+
+    def buy_tickets(self):
+        """Purchases a ticket through buy form. """
+        self.open(base_url)
+        self.type("#buyname", "Avengers Infinity War")
+        self.type("#buyquantity", "1")
+        self.click("#t-submit")
+
+    def test_register_login_create_ticket(self):
+        """ This test checks the implemented login/logout feature """
+        self.register()
+        self.login()
+        self.create_tickets()
+        self.open(base_url)
+        #should open base url
+        self.assert_element("#welcome-header")
+        #self.assert_text("Welcome test_frontend", "#welcome-header")
+        self.assert_element("#tickets div h4")
+        self.assert_text("Title: Avengers Infinity War, Price: 15, Quantity: 20, Expiration Date: 20210301")
+
+    def test_register_login_buy_ticket(self):
+        """ This test checks the implemented login/logout feature """
+        self.register()
+        self.login()
+        self.buy_tickets()
+        self.open(base_url)
+        #should open base url
+        self.assert_element("#welcome-header")
+        #self.assert_text("Welcome test_frontend", "#welcome-header")
+        self.assert_element("#tickets div h4")
+        self.assert_text("Title: Avengers Infinity War, Price: 15, Quantity: 19, Expiration Date: 20210711")
+        
